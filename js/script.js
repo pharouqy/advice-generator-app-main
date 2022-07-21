137c57e45556b1353cc25f74a5f239afdb593142
farouky <pharouky@gmail.com>
2022-07-21 14:07:09 +0100

    Finalisation du projet !!!

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
 function getData() {
+  window.addEventListener("DOMContentLoaded", (event) => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((data) => {
+        displayData(data);
+      })
+      .catch((err) => console.log(err));
+  });
   const btn = document.getElementsByTagName("button")[0];
   btn.addEventListener("click", () => {
     fetch("https://api.adviceslip.com/advice")
       .then((res) => res.json())
       .then((data) => {
-        const span = document.getElementById("Number");
-        span.textContent = data.slip.id;
-        const advice = document.getElementById("Advice");
-        advice.textContent = data.slip.advice;
+        displayData(data);
       })
       .catch((err) => console.log(err));
   });
 }
 
 getData();
+
+function displayData(data) {
+  const span = document.getElementById("Number");
+  span.textContent = data.slip.id;
+  const advice = document.getElementById("Advice");
+  advice.textContent = data.slip.advice;
+}
